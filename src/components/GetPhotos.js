dd7cac0ffa3733fe05be674f7e3430f065b59d17
Janne Piiroinen <janne.piiroinen1989@gmail.com>
2020-12-19 14:21:15 +0200

    välipäivitys, enää onClick puuttuu

--- src/components/GetPhotos.js
+++ src/components/GetPhotos.js
@@ -9,7 +9,7 @@ export default class GetPhotos extends React.Component {
 
   componentDidMount() {
     axios
-      .get(`http://jsonplaceholder.typicode.com/photos?_start=0&_limit=8`)
+      .get(`http://jsonplaceholder.typicode.com/photos?_start=0&_limit=10`)
       .then((res) => {
         console.log("response: ", res);
         this.setState({ photos: res.data });
@@ -18,34 +18,32 @@ export default class GetPhotos extends React.Component {
 
   render() {
     return (
+        <div class="container" style = {{ justifyContent: "center", marginTop: "150px", display: "flex" }}>
       <div class="row">
-        <div style={{ Width: "100vw", margin: "10px" }}>
+        <div style={{ Width: "100vw", margin: "7px" }}>
           {this.state.photos.map((photo, index) => {
             return (
               <div
                 style={{
                   Width: "20%",
-                  margin: "10px",
+                  margin: "8px",
                   height: "auto",
                   display: "inline-block",
+                  boxShadow: "5px 5px 5px black",
                 }}
                 key={index}
               >
-                <figure>
-                  <a
-                    target="_parent"
-                    href={photo.url}
-                  >
-                    <img src={photo.thumbnailUrl} alt={photo.url} />
-                    {this.state.photoId === photo.id && (
-                      <figcaption>{photo.title}</figcaption>
-                    )}
-                  </a>
-                </figure>
+                <a target="_parent" href={photo.url}>
+                  <img src={photo.thumbnailUrl} alt={photo.url} />
+                  {this.state.photoId === photo.id && (
+                    <figcaption>{photo.title}</figcaption>
+                  )}
+                </a>
               </div>
             );
           })}
         </div>
+      </div>
       </div>
     );
   }
